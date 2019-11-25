d174e5ec7d040d5d92b099326aea2451b6fcf765
naveenkrsh <naveen.kr.sh1993@gmail.com>
2019-11-25 20:49:14 +0530

    210.CourseSchedule.II

---
+++ LeetCode.Solutions/Graph/210.CourseSchedule.II/Solution.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Graph.CourseSchedule.II
+{
+    public class Solution
+    {
+        public int[] FindOrder(int numCourses, int[][] prerequisites)
+        {
+
+            var adlist = BuildAdjacencyList(numCourses, prerequisites);
+
+            var visted = new bool[numCourses];
+            var reOccur = new bool[numCourses];
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < numCourses; i++)
+            {
+                if (FindOrderUtil(i, adlist, visted, reOccur, stack))
+                {
+                    return new int[0];
+                }
+            }
+
+            var result = new int[numCourses];
+            for (int i = 0; i < numCourses; i++)
+            {
+                result[i] = stack.Pop();
+            }
+
+            return result;
+        }
+
+        private bool FindOrderUtil(int current, List<int>[] adlist, bool[] visted, bool[] reOccur, Stack<int> stack)
+        {
+
+            if (reOccur[current])
+                return true;
+
+            if (visted[current])
+                return false;
+
+            visted[current] = true;
+            reOccur[current] = true;
+
+            for (int i = 0; i < adlist[current].Count; i++)
+            {
+                var c = adlist[current][i];
+                if (FindOrderUtil(c, adlist, visted, reOccur, stack))
+                    return true;
+            }
+
+            reOccur[current] = false;
+            stack.Push(current);
+
+            return false;
+        }
+
+        private List<int>[] BuildAdjacencyList(int numCourses, int[][] prerequisites)
+        {
+            List<int>[] adjacencyList = new List<int>[numCourses];
+
+            for (int i = 0; i < numCourses; i++)
+            {
+                adjacencyList[i] = new List<int>();
+            }
+
+            for (int i = 0; i < prerequisites.Length; i++)
+            {
+                var e = prerequisites[i];
+                // u --> v
+                var u = e[1];
+                var v = e[0];
+
+                adjacencyList[u].Add(v);
+            }
+
+            return adjacencyList;
+        }
+    }
+}
---
+++ LeetCode.Tests/Graph/210.CourseSchedule.II/UnitTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Solutions.Graph.CourseSchedule.II;
+namespace LeetCode.Tests.Graph.CourseSchedule.II
+{
+    [TestClass]
+    public class UnitTests
+    {
+        Solution sol;
+        [TestInitialize]
+        public void Init()
+        {
+            sol = new Solution();
+        }
+
+        [TestMethod]
+        public void ShouldReturnOrderTestCase1()
+        {
+            /*
+            Input: 2, [[1,0]] 
+            Output: [0,1]
+            Explanation: There are a total of 2 courses to take. To take course 1 you should have finished   
+             course 0. So the correct course order is [0,1] .
+             */
+            var noc = 2;
+            var edgelist = new int[1][];
+            edgelist[0] = new int[2] { 1, 0 };
+
+            var result = sol.FindOrder(noc, edgelist);
+            var expected = new int[] { 0, 1 };
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnOrderTestCase2()
+        {
+            /*
+            Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+            Output: [0,1,2,3] or [0,2,1,3]
+            Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both     
+             courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. 
+             So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
+             */
+            var noc = 4;
+            var edgelist = new int[4][];
+            edgelist[0] = new int[2] { 1, 0 };
+            edgelist[1] = new int[2] { 2, 0 };
+            edgelist[2] = new int[2] { 3, 1 };
+            edgelist[3] = new int[2] { 3, 2 };
+
+            var result = sol.FindOrder(noc, edgelist);
+            var expected = new int[] {0, 2, 1, 3 };
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+    }
+}
