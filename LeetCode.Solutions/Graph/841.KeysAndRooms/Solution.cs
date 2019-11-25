8ded814a819b5506160f563b7ac4d71a63f3ea54
naveenkrsh <naveen.kr.sh1993@gmail.com>
2019-11-25 22:45:35 +0530

    841.KeysAndRooms

---
+++ LeetCode.Solutions/Graph/841.KeysAndRooms/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Solutions.Graph.KeysAndRooms
+{
+    public class Solution
+    {
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            var totalRooms = rooms.Count;
+            var visted = new bool[totalRooms];
+
+
+            CanVisitAllRoomsUtil(0, totalRooms, rooms, visted);
+
+
+            for (int i = 0; i < totalRooms; i++)
+            {
+                if (visted[i] == false)
+                    return false;
+            }
+            return true;
+        }
+
+        public void CanVisitAllRoomsUtil(int currentRoom, int totalRooms, IList<IList<int>> rooms, bool[] visted)
+        {
+            if (visted[currentRoom])
+                return;
+            visted[currentRoom] = true;
+            for (int i = 0; i < rooms[currentRoom].Count; i++)
+            {
+                CanVisitAllRoomsUtil(rooms[currentRoom][i], totalRooms, rooms, visted);
+            }
+        }
+    }
+}
---
+++ LeetCode.Tests/Graph/841.KeysAndRooms/UnitTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Solutions.Graph.KeysAndRooms;
+using System.Collections.Generic;
+
+namespace LeetCode.Tests.Graph.KeysAndRooms
+{
+    [TestClass]
+    public class UnitTests
+    {
+        Solution sol;
+        [TestInitialize]
+        public void TesTInit()
+        {
+            sol = new Solution();
+        }
+
+        [TestMethod]
+        public void ShouldVisitAllRoom()
+        {
+            //arrange
+            //Input: [[1],[2],[3],[]]
+            IList<IList<int>> input = new List<IList<int>>
+            {
+                new List<int> { 1 },
+                new List<int> { 2 },
+                new List<int> { 3 },
+                new List<int>()
+            };
+
+            //act
+            var result = sol.CanVisitAllRooms(input);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ShouldNotVisitAllRoom()
+        {
+            //arrange
+            //Input: [[1,3],[3,0,1],[2],[0]]
+            IList<IList<int>> input = new List<IList<int>>
+            {
+                new List<int> { 1,3 },
+                new List<int> { 3,0,1 },
+                new List<int> { 2 },
+                new List<int>{0}
+            };
+
+            //act
+            var result = sol.CanVisitAllRooms(input);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+    }
+}
