02c6e98764e084d6c46356ae9eaa1fed88e4153c
naveenkrsh <naveen.kr.sh1993@gmail.com>
2019-11-27 21:03:28 +0530

    739.DailyTemperatures

---
+++ LeetCode.Solutions/Stack/739.DailyTemperatures/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+namespace LeetCode.Solutions.Stack.DailyTemperatures
+{
+    public class Solution
+    {
+        public int[] DailyTemperatures(int[] T)
+        {
+            var total_temp = T.Length;
+            var ans = new int[total_temp];
+            var stack = new Stack<int>();
+
+            for (int i = total_temp - 1; i >= 0; i--)
+            {
+                int temp = i;
+                while (stack.Count != 0 && T[i] >=T[stack.Peek()])
+                {
+                    stack.Pop();
+                }
+
+                ans[i] = stack.Count == 0 ? 0 : stack.Peek() - i;
+                stack.Push(i);
+            }
+            return ans;
+        }
+    }
+}
---
+++ LeetCode.Tests/Stack/739.DailyTemperatures/UnitTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode.Solutions.Stack.DailyTemperatures;
+
+namespace LeetCode.Tests.Stack.DailyTemperatures
+{
+    [TestClass]
+    public class UnitTests
+    {
+        Solution sol;
+        [TestInitialize]
+        public void IntUnitTests()
+        {
+            sol = new Solution();
+        }
+
+        [TestMethod]
+        public void ShouldPass()
+        {
+            var T = new int[] { 73, 74, 75, 71, 69, 72, 76, 73 };
+
+            var expected = new int[] { 1, 1, 4, 2, 1, 1, 0, 0 };
+
+            var actual = sol.DailyTemperatures(T);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldPassTestCase2()
+        {
+            var T = new int[] { 90, 63, 71, 59, 48, 48, 47, 77, 101, 71 };
+
+            var expected = new int[] { 8, 1, 5, 4, 3, 2, 1, 1, 0, 0};
+
+            var actual = sol.DailyTemperatures(T);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
