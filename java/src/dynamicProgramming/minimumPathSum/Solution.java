20ce073f35ab9f3beb555ff004a25ccba6d05f07
naveenkrsh <naveen.kr.sh1993@gmail.com>
2019-12-17 12:52:40 +0530

    64. Minimum Path Sum. Without extra memory

--- java/src/dynamicProgramming/minimumPathSum/Solution.java
+++ java/src/dynamicProgramming/minimumPathSum/Solution.java
@@ -4,6 +4,10 @@ public class Solution {
 
     public int minPathSum(int[][] grid) {
 
+        return minPathSumWithoutExtraMemory(grid);
+    }
+
+    private int minPathSumExtraMemory(int[][] grid) {
         int m = grid.length;
         if (m == 0)
             return 0;
@@ -22,5 +26,24 @@ public class Solution {
             }
         }
         return dp[m - 1][n - 1];
+    }
+
+    private int minPathSumWithoutExtraMemory(int[][] grid) {
+        int m = grid.length;
+        if (m == 0)
+            return 0;
+        int n = grid[0].length;
+        for (int i = 1; i < n; i++) {
+            grid[0][i] = grid[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
+            grid[i][0] = grid[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+            }
+        }
+        return grid[m - 1][n - 1];
     }
 }
